f3a0e6513efb0cb19cb412645013eed0d4210051
wilromeo <wilfriedkp.romeo@mail.com>
2020-11-17 23:40:46 +0100

    Topic fonction CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    def authenticate_user
+      if @current_user == nil
+        flash[:notice] = t('notice.login_needed')
+        redirect_to new_session_path
+      end
+    end
 end
