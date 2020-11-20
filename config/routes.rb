43e83c78663aa7b0de0feade6562b88401b17e38
wilromeo <wilfriedkp.romeo@mail.com>
2020-11-20 15:20:47 +0100

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'forums#index', as: :root
+
+  resources :forums,:except=>[:show] do
+    resources :topics do 
+      resources :comments,:only=>[:create,:destroy]
+    end
+  end 
 end
